18933c06b5f24ee8e99e7b5cf7a87b6115cfb546
Asger Behncke Jacobsen <abj@Asgers-MacBook-Pro-2.local>
2014-09-26 15:42:07 +0200

    date range tag

---
+++ lib/date_time_tags/tag/date_range.rb
@@ -0,0 +1,57 @@
+module DateTimeTags
+  class Tag
+    class DateRange < Tag
+
+      def initialize date_from, date_to=nil, options={}
+        @date_from = date_from
+        @date_to = date_to
+        @options = options
+      end
+
+      # ---------------------------------------------------------------------
+
+      def spans_years?
+        return false if @date_to.nil?
+        @date_from.year != @date_to.year
+      end
+
+      def spans_months?
+        @date_from.month != @date_to.month
+      end
+
+      def within_a_week?
+        (@date_from - @date_to) <= 7
+      end
+
+      def one_day?
+        @date_from == @date_to
+      end
+
+      # ---------------------------------------------------------------------
+
+      def dom_classes
+        res = []
+        res << 'date_range'
+        res << 'same_year' unless spans_years?
+        res << 'same_month' unless spans_months?
+        res << 'more_than_a_week' unless within_a_week?
+        res << 'same_day' if one_day?
+        res
+      end
+
+      # ---------------------------------------------------------------------
+
+      def to_html
+        separator = @options.fetch(:separator, ' – ')
+        content_tag(:span, class: dom_classes) do
+          [
+            DateTimeTags::Tag::Date.new(@date_from, :time, class: 'from').to_html,
+            content_tag(:span, separator, class: 'date_range_separator'),
+            DateTimeTags::Tag::Date.new(@date_to, :time, class: 'to').to_html
+          ].join.html_safe
+        end.html_safe
+      end
+
+    end
+  end
+end
