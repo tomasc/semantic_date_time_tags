394a0fdfb1b9d940721247516718dbc61d50ca5c
Asger Behncke Jacobsen <abj@Asgers-MacBook-Pro-2.local>
2014-09-26 15:41:54 +0200

    date/time format parser

---
+++ test/date_time_tags/format_parser_test.rb
@@ -0,0 +1,17 @@
+require 'date_time_tags/format_parser'
+require 'test_helper'
+
+module DateTimeTags
+  describe FormatParser do
+
+    describe '#to_html' do
+      it 'wraps the components into span tags' do
+        format = '%d / %m / %Y'
+        string = '12 / 12 / 2014'
+        res = FormatParser.new(format, string).to_html
+        res.must_equal '<span class="day d">12</span> <span class="sep">/</span> <span class="month m">12</span> <span class="sep">/</span> <span class="year Y">2014</span>'
+      end
+    end
+
+  end
+end
