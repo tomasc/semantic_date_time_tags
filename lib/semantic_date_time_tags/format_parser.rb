496389f6bce9da6a0d2d6975562566cefb6c744b
Tomas Celizna <tomas.celizna@gmail.com>
2016-07-31 22:05:52 +0200

    make sure complete string is preserved

--- lib/semantic_date_time_tags/format_parser.rb
+++ lib/semantic_date_time_tags/format_parser.rb
@@ -7,14 +7,16 @@ module SemanticDateTimeTags
 
     def to_html
       processed_str = str
-      formatting_components.flatten.inject('') do |res, comp|
-        regexp = Regexp.new(get_regexp_for_component(comp))
-        if match = processed_str.match(regexp)
-          res += get_tag_for_match(match[0], comp)
-          processed_str = processed_str[match[0].length..-1]
-        end
-        res
-      end.html_safe
+      (
+        formatting_components.flatten.inject('') do |res, comp|
+          regexp = Regexp.new(get_regexp_for_component(comp))
+          if match = processed_str.match(regexp)
+            res += get_tag_for_match(match[0], comp)
+            processed_str = processed_str[match[0].length..-1]
+          end
+          res
+        end + processed_str
+      ).html_safe
     end
 
     private # =============================================================
--- test/semantic_date_time_tags/format_parser_test.rb
+++ test/semantic_date_time_tags/format_parser_test.rb
@@ -70,5 +70,14 @@ describe SemanticDateTimeTags::FormatParser do
         to_html.must_equal "<span class=\"day A\">Čtvrtek</span><span class=\"sep\">, </span><span class=\"day d\">16</span><span class=\"sep\">. </span><span class=\"month B\">Červen</span><span class=\"sep\">, </span><span class=\"year Y\">2016</span>"
       end
     end
+
+    describe 'additional string' do
+      let(:format) { '%d.%m.%Y %H.%M hrs' }
+      let(:string) { "09.09.2014 19.00 hrs" }
+
+      it 'preserves additional strings' do
+        to_html.must_equal "<span class=\"day d\">09</span><span class=\"sep\">.</span><span class=\"month m\">09</span><span class=\"sep\">.</span><span class=\"year Y\">2014</span><span class=\"sep\"> </span><span class=\"hours H\">19</span><span class=\"sep\">.</span><span class=\"minutes M\">00</span> hrs"
+      end
+    end
   end
 end
